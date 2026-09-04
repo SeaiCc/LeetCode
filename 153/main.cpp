bb1f01e4f36192e0f412ce79c49f94c524ce7440
SeaiCc <taocui56@gmail.com>
2026-09-04 15:54:30 +0000

    2026.09.04

---
+++ 153/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int findMin(vector<int>& nums) {
+    int n = nums.size();
+
+    if(n == 1) return nums[0];
+
+    int l = 0, r = n-1;
+    while(l < r) {
+      int mid = l + (r-l) / 2;
+
+      if(nums[mid] < nums[0]) {
+        // mid 右侧的可以排除
+        r = mid;
+      }else if (nums[mid] > nums[n-1]){
+        // mid 左侧可以排除
+        l = mid + 1;
+      }else {
+        r = mid -1;
+      }
+    }
+
+    return nums[l];
+  }
+};
